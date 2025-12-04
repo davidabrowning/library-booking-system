e1a499bd5fab6deb9a37bce26d8201386867d231
David Browning <david.a.browning@gmail.com>
2025-12-04 13:07:19 +0100

    Add IRepository.

---
+++ LibraryBookingSystem/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryBookingSystem
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        bool Exists(int id);
+        T? Get(int id);
+        IEnumerable<T> GetAll();
+        void Add(T t);
+        void Update(T t);
+        void Delete(T t);
+    }
+
+    public interface IEntity
+    {
+        int Id { get; }
+    }
+}

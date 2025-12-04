c779de367bdd79331160a863aea99d49746620c1
David Browning <david.a.browning@gmail.com>
2025-12-04 11:03:59 +0100

    Add Library method placeholders.

--- LibraryBookingSystem/Library.cs
+++ LibraryBookingSystem/Library.cs
@@ -4,5 +4,25 @@
     {
         private List<Book> _books = new();
         public List<Book> Books { get { return _books.ToList(); } }
+
+        public void AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book FindBookByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoanBook(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnBook(string isbn)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
